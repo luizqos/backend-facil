2dd1303017a466b7c0c55bfe0d1085f66b14e348
Luiz Silva <luiz.silva@grupozelo.com>
2023-03-31 23:13:48 -0300

    Implementado Repository em produtos

--- src/routes/produto.routes.js
+++ src/routes/produto.routes.js
@@ -3,26 +3,7 @@ module.exports = app => {
   
     let router = require("express").Router();
   
-    // Create a new Tutorial
-    router.post("/", produtos.create);
-  
-    // Retrieve all clientes
-    router.get("/", produtos.findAll);
-  
-    // Retrieve all published clientes
-    router.get("/published", produtos.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
-    router.get("/:id", produtos.findOne);
-  
-    // Update a Tutorial with id
-    router.put("/:id", produtos.update);
-  
-    // Delete a Tutorial with id
-    router.delete("/:id", produtos.delete);
-  
-    // Delete all clientes
-    router.delete("/", produtos.deleteAll);
+    router.get("/", produtos.buscaTodosProdutos);
   
     app.use('/api/produtos', router);
   };
