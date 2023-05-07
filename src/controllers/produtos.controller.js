664ae202904455c2f2a1a60b5e48ae115cb59e27
Luiz Silva <luiz.silva@grupozelo.com>
2023-05-06 22:46:43 -0300

    Implementando busca com query dinamica

--- src/controllers/produtos.controller.js
+++ src/controllers/produtos.controller.js
@@ -1,17 +1,28 @@
+/* eslint-disable no-prototype-builtins */
 const produtosRepository = require('../repositories/produtos.repository')
 const { Op } = require('sequelize')
-
 class ProdutosController {
     async buscaProdutos(req, res) {
-        const { api } = req.query
-        let filtrosWhere = { api: api }
-        if (!api) {
-            filtrosWhere = { idProdutos: { [Op.gt]: 0 } }
+        try {
+            const queryParams = req.query
+            const filtrosWhere = {}
+
+            Object.keys(queryParams).map((key) => {
+                filtrosWhere[key] = { [Op.like]: `%${queryParams[key]}%` }
+            })
+            const produtos =
+                await produtosRepository.buscaProdutosDinamicamente(
+                    filtrosWhere
+                )
+            if (!produtos.length) {
+                return res.status(204).send()
+            }
+
+            return res.send(produtos)
+        } catch (error) {
+            console.log(error)
+            return res.status(500).send({ message: 'Internal server error' })
         }
-        const produtos = await produtosRepository.buscaProdutosDinamicamente(
-            filtrosWhere
-        )
-        return res.send(produtos)
     }
 }
 
